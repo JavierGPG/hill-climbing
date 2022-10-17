6824c7d7c14c05d20ef6911cfa5d4ad8e9cedbc0
JavierGPG <jgpastranag@correo.udistrital.edu.co>
2022-10-16 20:49:45 -0500

    Update nodo.js

--- nodo.js
+++ nodo.js
@@ -1,21 +1,15 @@
-class Nodo
-{
-    constructor(nombre, valorDestino, hijo)
-    {
+class Nodo {
+    constructor(nombre, valorDestino, hijo) {
         this.nombre = nombre;
         this.valorDestino = valorDestino;
         this.hijo = hijo;
     }
 }
 
-arbolin = new Nodo("A",7, new Array(
-    [new Nodo("B",6,new Array(
-        [new Nodo("D", 3, null)])), 
-    new Nodo("C",4,null)]));
+arbolin = new Nodo("A", 7, new Array([new Nodo("B", 6, new Array([new Nodo("D", 3, null)])), new Nodo("C", 4, null)]));
 
 
-function busqueda(inicio, destino, arbol)
-{
+function busqueda(inicio, destino, arbol) {
     //busca el nodo de incio en el array
     //mira los hijos de incio arbol.hijo 
     //si el hijo es igual a null es una hoja
@@ -23,10 +17,17 @@ function busqueda(inicio, destino, arbol)
     //el menor valor a destino y sigue por ahi
     //let recorrido = new Array(); 
     //recorrido.push(i.nombre) <- guadra el recorrido en un array
-    /*arbol.forEach(i => {
-        if(i.nombre == inicio)
-        {
-
+    let recorrido = new Array();
+    arbol.forEach(i => {
+        if (i.nombre == inicio) {
+            if (arbol.hijo != null) {
+                recorrido.push(i.nombre);
+            } else {
+                if (arbol.hijo == null || inicio.nombre == destino) {
+                    recorrido.push(i.nombre);
+                }
+            }
         }
-    });*/
+    });
+    return recorrido;
 }
